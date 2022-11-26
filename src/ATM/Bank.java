de1adca65e5cc794a0fc864ab5ef4c7401913da8
Bilal <bilal.ch.2554@gmail.com>
2022-11-26 16:34:13 +0500

    Bank Class Implemented

--- src/ATM/Bank.java
+++ src/ATM/Bank.java
@@ -1,5 +1,26 @@
 package ATM;
 
 public class Bank {
-
+	private int code;
+	private String address;
+	
+	public int getCode() { return code; }
+	public void setCode(int code) { this.code = code; }
+	
+	public String getAddress() { return address; }
+	public void setAddress(String address) { this.address = address; }
+	
+	public Bank() {
+		setCode(0);
+		setAddress("None");
+	}
+	
+	public Bank(int code, String address) {
+		setCode(code);
+		setAddress(address);
+	}
+	
+	public void manage() { }
+	
+	public void maintain() { }
 }
