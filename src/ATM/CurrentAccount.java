5bae05918eb2c04ef9047c66d1a9ee1ec4c86b25
Bilal <bilal.ch.2554@gmail.com>
2022-11-27 22:11:44 +0500

    Current and Savings account classes implemented

--- src/ATM/CurrentAccount.java
+++ src/ATM/CurrentAccount.java
@@ -1,5 +1,29 @@
 package ATM;
 
-public class CurrentAccount {
-
+public class CurrentAccount extends Account{
+	public CurrentAccount() {
+		super(0, 0);
+	}
+	
+	public CurrentAccount(int number, double balance) {
+		super(number, balance);
+	}
+	
+	public void deposit(double amount) {
+			System.out.println("Deposit Successful");
+			super.setBalance(super.getBalance() + amount);
+	}
+	
+	public void withdraw(double amount) {
+		if(this.getBalance() < amount) {
+			System.out.println("Insufficient Balance");
+		} else {
+			System.out.println("Withdraw Successful");
+			super.setBalance(super.getBalance() - amount);
+		}
+	}
+		
+	public void displayBalance() {
+		System.out.println("Balance in your Account is:" + super.getBalance());
+	}
 }
--- src/ATM/SavingsAccount.java
+++ src/ATM/SavingsAccount.java
@@ -1,5 +1,20 @@
 package ATM;
 
-public class SavingsAccount {
-
+public class SavingsAccount extends Account{
+	public SavingsAccount() {
+		super(0, 0);
+	}
+	
+	public SavingsAccount(int number, double balance) {
+		super(number, balance);
+	}
+	
+	public void deposit(double amount) {
+			System.out.println("Deposit Successful");
+			super.setBalance(super.getBalance() + amount);
+	}
+		
+	public void displayBalance() {
+		System.out.println("Balance in your Account is:" + super.getBalance());
+	}
 }
